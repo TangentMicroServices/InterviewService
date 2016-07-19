c6f482da7c783406ce93857348032afd2ffccb9f
Christiaan Vermeulen <chris@cmsoft.co.za>
2016-07-19 15:32:44 +0200

    add models, migrations and app include for api service

--- api/models.py
+++ api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return '%s' % self.name
+
+class Question(models.Model):
+	label = models.CharField(max_length=200)
+	rows = models.IntegerField(default=1)
+	category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+	def __init__(self, arg):
+		super(Question, self).__init__()
+		self.arg = arg
+
+
+
+
+		
