a769bced6dbd0cae4dbedd228f512d401dbc9e63
Christiaan Vermeulen <chris@cmsoft.co.za>
2016-07-19 16:11:15 +0200

    initial crud for category and questions

--- api/models.py
+++ api/models.py
@@ -5,20 +5,28 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
 
-    def __unicode__(self):
-        return '%s' % self.name
+    def __str__ (self):
+    	return self.name
+
+    @property
+    def question_count (self):
+    	return Question.objects.filter(category_id=self.id).count()
+
+    class Meta:
+    	verbose_name = "Category"
+    	verbose_name_plural = "Categories"
 
 class Question(models.Model):
-	label = models.CharField(max_length=200)
+	name = models.CharField(max_length=200)
+	category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="category")
 	rows = models.IntegerField(default=1)
-	category = models.ForeignKey(Category, on_delete=models.CASCADE)
+	answer = models.TextField()
+	sequence = models.IntegerField(default=1)
 
-	def __init__(self, arg):
-		super(Question, self).__init__()
-		self.arg = arg
+	class Meta:
+		ordering = ["sequence"]
 
-
-
-
-		
+	def __str__ (self):
+		return self.name
